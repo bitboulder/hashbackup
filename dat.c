31d149c818a0e5f6c02c90eb6a04057995c2593d
Frank Duckhorn <git@bitboulder.net>
2015-05-27 21:55:23 +0200

    mkdir in dat

--- dat.c
+++ dat.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #include "dat.h"
 #include "main.h"
 #include "db.h"
 
 #define BUFLEN	(1024*1024)
-void datadd(const unsigned char *sha,const char *fn){
-	char ffn[FNLEN],ffo[FNLEN];
-	FILE *fdi,*fdo;
-	snprintf(ffo,FNLEN,DH "/%02x/%02x/%016lx%016lx%02x",
+
+void shafn(const unsigned char *sha,char *fn){
+	snprintf(fn,FNLEN,DH "/%02x/%02x/%016lx%016lx%02x",
 		sha[0],sha[1],
 		*(uint64_t*)(sha+2),
 		*(uint64_t*)(sha+10),
 		*(uint16_t*)(sha+18));
-	if((fdo=fopen(ffo,"rb"))){ fclose(fdo); return; }
-	snprintf(ffn,FNLEN,"%s/%s",dbbdir(),fn);
-	if(!(fdi=fopen(ffn,"rb"))){ error(1,"file open failed for '%s'",ffn); return; }
-	snprintf(ffn,FNLEN,"mkdir -p " DH "/%02x",sha[0]); system(ffn);
-	snprintf(ffn,FNLEN,"mkdir -p " DH "/%02x/%02x",sha[0],sha[1]); system(ffn);
-	if(!(fdo=fopen(ffo,"wb"))){ error(1,"file open failed for '%s'",ffo); return; }
+}
+
+void mkd(const char *fn){
+	int i;
+	char dn[FNLEN];
+	for(i=0;i<FNLEN && fn[i];i++){
+		if(i && fn[i]=='/' && stat(dn,NULL)) mkdir(dn,0777); /* TODO check */
+		dn[i]=fn[i];
+	}
+}
+
+void copyfile(const char *fni,const char *fno){
+	FILE *fdi,*fdo;
+	mkd(fno);
+	if(!(fdi=fopen(fni,"rb"))){ error(1,"file open failed for '%s'",fni); return; }
+	if(!(fdo=fopen(fno,"wb"))){ error(1,"file open failed for '%s'",fno); return; }
 	while(!feof(fdi)){
 		char buf[BUFLEN];
 		size_t r=fread(buf,1,BUFLEN,fdi);
@@ -30,22 +42,17 @@ void datadd(const unsigned char *sha,const char *fn){
 	fclose(fdo);
 }
 
-void datget(const unsigned char *sha,const char *ffo){
-	char ffi[FNLEN];
-	FILE *fdi,*fdo;
-	snprintf(ffi,FNLEN,DH "/%02x/%02x/%016lx%016lx%02x",
-		sha[0],sha[1],
-		*(uint64_t*)(sha+2),
-		*(uint64_t*)(sha+10),
-		*(uint16_t*)(sha+18));
-	if(!(fdi=fopen(ffi,"rb"))){ error(1,"file open failed for '%s'",ffi); return; }
-	if(!(fdo=fopen(ffo,"wb"))){ error(1,"file open failed for '%s'",ffo); return; }
-	while(!feof(fdi)){
-		char buf[BUFLEN];
-		size_t r=fread(buf,1,BUFLEN,fdi);
-		fwrite(buf,1,r,fdo);
-	}
-	fclose(fdi);
-	fclose(fdo);
+void datadd(const unsigned char *sha,const char *fn){
+	char fni[FNLEN],fno[FNLEN];
+	shafn(sha,fno);
+	if(!stat(fno,NULL)) return; /* TODO check */
+	snprintf(fni,FNLEN,"%s/%s",dbbdir(),fn);
+	copyfile(fni,fno);
+}
+
+void datget(const unsigned char *sha,const char *fno){
+	char fni[FNLEN];
+	shafn(sha,fni);
+	copyfile(fni,fno);
 }
 
